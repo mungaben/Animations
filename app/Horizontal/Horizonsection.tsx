fbaad2d1f889d49b4031f4009e944a1e9d0a9ec4
mungaben <kamaumungai3742@gmail.com>
2023-09-04 23:21:48 +0300

    design : -webkit-box-reflect: below

--- app/Horizontal/Horizonsection.tsx
+++ app/Horizontal/Horizonsection.tsx
@@ -13,13 +13,13 @@ const Horizonsection = () => {
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-90%"]);
   return (
     <section className=" h-[500dvh] bg-background" ref={targetref}>
-      <div className=" sticky top-0 h-screen flex bg-muted-foreground items-center overflow-hidden min-w-full ">
-        <motion.div className=" flex gap-4 min-w-full " style={{
+      <div className=" sticky top-0 h-screen flex bg-muted-foreground items-center overflow-hidden min-w-full  ">
+        <motion.div className=" flex gap-4 min-w-full snap-x " style={{
             x
         }}>
          {
             Array(50).fill(0).map((_,i)=><div>
-                <Image src={`https://picsum.photos/200/300?random=${i}`} width={500} height={500} alt={"random image"} className=" min-h-[500px] flex flex-1 w-[50dvw] min-w-[300px]" />
+                <Image src={`https://picsum.photos/200/300?random=${i}`} width={500} height={500} alt={"random image"} className=" min-h-[500px] flex flex-1 w-[50dvw] min-w-[300px]  scroll-ml-6 inveted  "  />
             </div>
             )
          }
