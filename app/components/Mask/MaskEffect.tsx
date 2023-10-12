454edd964bca1eb133a4ccc023ef93dc68c57150
mungaben <kamaumungai3742@gmail.com>
2023-10-13 00:21:58 +0300

    ::mask effect

--- app/components/Mask/MaskEffect.tsx
+++ app/components/Mask/MaskEffect.tsx
@@ -1,34 +1,28 @@
-
-
-
-import React from 'react'
-
+import React from "react";
 
 const MaskEffect = () => {
   return (
-    <main className="main h-[200dvh] relative  " >
-        <div className=" absolute  top-0  mask">
-            <div className=' body'>
-            <p className=' '>
-            Passionate developer and designer creating innovative digital solutions. Specializing in Next.js and SCSS. Crafting seamless user experiences with a keen eye for detail. 
-
-            </p>
-
-
-            </div>
-         
+    <main className="main h-[200dvh] relative  ">
+      <div className=" absolute  top-0  mask">
+        <div className=" body">
+          <p className=" ">
+            Passionate developer and designer creating innovative digital
+            solutions. Specializing in Next.js and SCSS. Crafting seamless user
+            experiences with a keen eye for detail.
+          </p>
         </div>
-        <div className=' absolute  top-0' >
-            <div className=" body ">
-                <p className=' '>
-                Full-stack developer specializing in Next.js and SCSS. Passionate about creating efficient and user-friendly web applications. Let's build something amazing together
-                </p>
-          
-            </div>
+      </div>
+      <div className=" absolute  top-0">
+        <div className=" body ">
+          <p className=" ">
+            Full-stack developer specializing in Next.js and SCSS. Passionate
+            about creating efficient and user-friendly web applications. Let's
+            build something amazing together
+          </p>
         </div>
-
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default MaskEffect
+export default MaskEffect;
